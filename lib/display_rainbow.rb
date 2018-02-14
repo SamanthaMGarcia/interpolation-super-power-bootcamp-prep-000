c6775e1db3ddc6c4846df61cadfb38e22319740e
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-02-14 14:18:56 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
-def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+def display_rainbow(["red", "orange", "yellow", "green", "blue", "indigo", "violet"])
   puts "R: [0], O: [1], Y: [2], G: [3], I: [4], V: [5]"
 end
